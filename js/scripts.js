5ecb9a2ee00f41740b3fefe116da7a0da57df03b
Jordan Taylor <jordan22taylor@gmail.com>
2022-02-23 17:24:55 -0700

    checkpoint

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,45 @@ fetch(apiURL)
     
     
 }); //end of "then" fat arrow function
+
+
+
+
+
+
+
+
+let theImageTag = document.createElement("img");
+theImageTag.src=myList[i].photo;
+console.log(myList[i].photo);
+
+let theHotelTag = document.createElement("figcaption");
+theHotelTag.textContent = myList[i].name;
+console.log(myList[i].name);
+
+let theIconLetTag = document.createElement("i");
+theIconLeftTag.className = "icon ion-md-car";
+
+let theAddress1Tag = document.createElement("p");
+theAddress1Tag.textContent = myList[i].address[0];
+console.log(myList[i].address[0]);
+
+let theAddress2Tag = document.createElement("p");
+theAddress2Tag.textContent = myList[i].address[1];
+console.log(myList[i].address[1]);
+
+let theIconRightTag = document.createElement("i");
+theIconRightTag.className = "icon ion-md-call";
+
+let thePhoneTag = document.createElement("p");
+thePhoneTag.textContent = myList[i].phone;
+console.log(myList[i].phone);
+
+let theSpanLeftTag = document.createElement("span");
+theSpanLeftTag.appendChild(theIconLeftTag);
+theSpanLeftTag.appendChild(theAddress1Tag);
+theSpanLeftTag.appendChild(theAddress2Tag);
+
+let theSpanRightTag = document.createElement("span");
+theSpanRightTag.appendChild(theIconRightTag);
+theSpanRightTag.appendChild(thePhoneTag);
